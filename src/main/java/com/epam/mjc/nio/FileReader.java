f2ee24866b2e63c44f6d18f906d99261f6078121
ManapYereke <manap.ereke@gmail.com>
2023-06-02 00:38:54 +0600

    edchb

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -2,28 +2,21 @@ package com.epam.mjc.nio;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.HashMap;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        Path filePatch = file.toPath();
         try {
-            Map<String, String> fd = new HashMap<>();
-            Files.lines(filePatch, StandardCharsets.UTF_8).forEach(line -> {
-                String[] str = line.split(":");
-                if (str.length == 2)
-                    fd.put(str[0].trim(), str[1].trim());
-            });
-            String name = fd.get("Name");
-            int age = Integer.parseInt(fd.get("Age"));
-            String email = fd.get("Email");
-            long phone = Long.parseLong(fd.get("Phone"));
-            System.out.println(".close()");
+            Path filePath = file.toPath();
+            BufferedReader reader = Files.newBufferedReader(filePath);
+            String name = reader.readLine().split(":")[1].trim();
+            int age = Integer.parseInt(reader.readLine().split(":")[1].trim());
+            String email = reader.readLine().split(":")[1].trim();
+            long phone = Long.parseLong(reader.readLine().split(":")[1].trim());
+            reader.close();
             return new Profile(name, age, email, phone);
         } catch (IOException e) {
             e.printStackTrace();
